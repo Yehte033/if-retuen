427be7a72508df1f762c85b9ee8f50c20278baa6
erict <erict@MSI>
2023-03-19 09:58:00 +0800

    加入專案檔案。

---
+++ 遞迴程式/遞迴程式.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int f(int n) {
+    if (n <= 1) return n + 1;
+    else return f(n - 1) + f(floor(n/2.0));
+}
+int main()
+{
+    int n;
+    while (cin >> n) {
+    cout << f(n) << endl;
+    }
+
+        
+    return 0;
+}
+
